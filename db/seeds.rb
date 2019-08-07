edeac3216c5bfd2770e0aa3570cf866c0888884b
Benjamin Watrinet <watrinet@gmail.com>
2019-08-07 23:11:50 +0100

    my first crud app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name: "La Tour d'Argent",
+    address: "15 Quai de la Tournelle, 75005 Paris",
+    phone_number: "01 43 54 23 31",
+    category: "french"
+  },
+  {
+    name: 'Iloli',
+    address: '7 Rue Najib Mahfoud, Casablanca 20250',
+    phone_number: "06 08 86 66 33",
+    category: "japanese"
+  },
+  {
+    name: 'Golden China',
+    address: '12 Rue el Oraibi Jilali, Casablanca 20250',
+    phone_number: "05 22 27 35 26",
+    category: "chinese"
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
